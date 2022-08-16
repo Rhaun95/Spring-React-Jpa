9b89a970fda50089a72497033383802c9e4a526e
Soo <105895293+Rhaun95@users.noreply.github.com>
2022-08-16 12:13:21 +0900

    test

---
+++ book_security/src/test/java/com/example/book_security/web/BookControllerUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.book_security.web;
+
+
+//단위 테스트(Controller, Filter, ControllerAdvice)
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public class BookControllerUnitTest {
+
+
+}
